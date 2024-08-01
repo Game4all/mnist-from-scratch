f0f8920eb18dfc0e82f75de89586d6fc49b4d3b5
Lucas A. <game4allyt@gmail.com>
2024-08-01 11:57:22 +0200

    Add workflow file + rename html file

--- get-mnist.sh
+++ get-mnist.sh
@@ -7,7 +7,7 @@ MNIST_URL_TEST_IMAGES="https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images
 MNIST_URL_TEST_LABELS="https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz"
  
 # Define the download directory
-DOWNLOAD_DIR="/mnist"
+DOWNLOAD_DIR="./mnist"
 
 # Create the download directory if it doesn't exist
 mkdir -p $DOWNLOAD_DIR
@@ -19,10 +19,10 @@ wget $MNIST_URL_TEST_IMAGES -P $DOWNLOAD_DIR
 wget $MNIST_URL_TEST_LABELS -P $DOWNLOAD_DIR
 
 # Unzip the files to root directory
-gunzip -c $DOWNLOAD_DIR/train-images-idx3-ubyte.gz > /train-images.idx3-ubyte
-gunzip -c $DOWNLOAD_DIR/train-labels-idx1-ubyte.gz > /train-labels.idx1-ubyte
-gunzip -c $DOWNLOAD_DIR/t10k-images-idx3-ubyte.gz > /t10k-images.idx3-ubyte
-gunzip -c $DOWNLOAD_DIR/t10k-labels-idx1-ubyte.gz > /t10k-labels.idx1-ubyte
+gunzip -c $DOWNLOAD_DIR/train-images-idx3-ubyte.gz > ./train-images.idx3-ubyte
+gunzip -c $DOWNLOAD_DIR/train-labels-idx1-ubyte.gz > ./train-labels.idx1-ubyte
+gunzip -c $DOWNLOAD_DIR/t10k-images-idx3-ubyte.gz > ./t10k-images.idx3-ubyte
+gunzip -c $DOWNLOAD_DIR/t10k-labels-idx1-ubyte.gz > ./t10k-labels.idx1-ubyte
 
 # Clean up the downloaded .gz files
 rm -rf $DOWNLOAD_DIR
