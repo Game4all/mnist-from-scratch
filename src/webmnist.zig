82c6b6d4664f7a738b0e1ba15e77821ea0a864fa
Lucas A. <game4allyt@gmail.com>
2024-08-06 18:41:43 +0200

    Introduce ability for continuous learning on model

--- src/webmnist.zig
+++ src/webmnist.zig
@@ -13,10 +13,22 @@ const MODEL_WEIGHTS = @embedFile("model.bin");
 
 /// Model itself.
 var model: MNISTClassifier(1) = undefined;
+
+// =================== Model in / out tensors =================
+
 /// Tensor containing the inputs.
 var input_tensor: Tensor(f32) = undefined;
 /// Results tensor.
 var argmax_result: Tensor(usize) = undefined;
+
+// ============ Tensors for backprop ===============
+
+/// Loss gradient tensor.
+var loss_grad: Tensor(f32) = undefined;
+/// One hot encoded tensor with the expect class logit.
+var expected_value: Tensor(f32) = undefined;
+/// The softmaxed model output logits.
+var softmaxed_values: Tensor(f32) = undefined;
 
 /// Initializes the model and loads its weights for inferrence.
 pub export fn init() u32 {
@@ -29,6 +41,10 @@ pub export fn init() u32 {
 
     input_tensor = Tensor(f32).init(model.inputShape(), allocator) catch return 1;
     argmax_result = Tensor(usize).init(.{ 1, 0, 1 }, allocator) catch return 1;
+
+    loss_grad = Tensor(f32).init(model.outputShape(), allocator) catch return 1;
+    softmaxed_values = Tensor(f32).init(model.outputShape(), allocator) catch return 1;
+    expected_value = Tensor(f32).init(model.outputShape(), allocator) catch return 1;
 
     return 0;
 }
@@ -49,4 +65,22 @@ pub export fn runInference() u32 {
     const result = model.forward(DummyDevice, &input_tensor) catch return 0;
     brainz.ops.argmax(f32, result, 1, &argmax_result);
     return @truncate(argmax_result.get(.{ 0, 0, 0 }));
+}
+
+/// Trains the model on the current drawing in the input tensor
+pub export fn train(expected: usize) u32 {
+    expected_value.fill(0.0);
+    expected_value.set(.{ 0, expected, 0 }, 1.0);
+
+    for (0..25) |_| {
+        const result = model.forward(DummyDevice, &input_tensor) catch return 0;
+
+        brainz.ops.softmax(f32, DummyDevice, result, 1, &softmaxed_values) catch return 0;
+        brainz.ops.categoricalCrossEntropyLossBackprop(f32, DummyDevice, &softmaxed_values, &expected_value, &loss_grad) catch return 0;
+
+        model.backwards(DummyDevice, &loss_grad) catch return 0;
+        model.step(DummyDevice, &input_tensor, 0.05) catch return 0;
+    }
+
+    return 0;
 }
