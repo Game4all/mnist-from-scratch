6063aa365155c7c36c63c9e4a28145d90b949b3d
Lucas A. <game4allyt@gmail.com>
2026-01-09 14:40:47 +0100

    Update build.zig to zig 0.15.2

--- build.zig
+++ build.zig
@@ -2,36 +2,44 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const brainz = b.dependency("brainz", .{
-        .optimize = .ReleaseFast,
+        .optimize = optimize,
         .target = target,
     });
 
     // code for model training
-    const train_model = b.addExecutable(.{
-        .name = "train",
+    const train_module = b.addModule("train_module", .{
         .root_source_file = b.path("src/train.zig"),
         .target = target,
-        .optimize = .ReleaseFast,
+        .optimize = optimize,
+    });
+
+    const train_executable = b.addExecutable(.{
+        .name = "train",
+        .root_module = train_module,
     });
 
     // code for inference on web with WASM
     const webmnist = b.addExecutable(.{
         .name = "webmnist",
-        .optimize = .ReleaseFast,
-        .target = b.resolveTargetQuery(.{ .cpu_arch = .wasm32, .os_tag = .freestanding }),
-        .root_source_file = b.path("src/webmnist.zig"),
+        .root_module = b.addModule("webmnist", .{
+            .root_source_file = b.path("src/webmnist.zig"),
+            .optimize = optimize,
+            .target = b.resolveTargetQuery(.{ .cpu_arch = .wasm32, .os_tag = .freestanding }),
+        }),
     });
 
     webmnist.rdynamic = true;
     webmnist.entry = .disabled;
     webmnist.export_memory = true;
 
-    train_model.root_module.addImport("brainz", brainz.module("brainz"));
+    // handle imports
+    train_executable.root_module.addImport("brainz", brainz.module("brainz"));
     webmnist.root_module.addImport("brainz", brainz.module("brainz"));
 
-    b.installArtifact(train_model);
+    b.installArtifact(train_executable);
 
     // copy html + wasm to the web directory
     b.installDirectory(.{ .source_dir = b.path("public"), .install_dir = .{ .custom = "web" }, .install_subdir = "public" });
@@ -44,7 +52,7 @@ pub fn build(b: *std.Build) void {
 
     // step to train model locally
     const run_train_cmd = b.step("train", "Runs model training");
-    run_train_cmd.dependOn(&b.addRunArtifact(train_model).step);
+    run_train_cmd.dependOn(&b.addRunArtifact(train_executable).step);
 
     // run model training if model checkpoints aren't found.
     std.fs.cwd().access("src/model.bin", .{}) catch |err| {
